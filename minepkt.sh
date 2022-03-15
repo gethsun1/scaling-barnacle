b365eb42ccde26f74e473dea34656687eef4adba
gethsun1 <56596995+gethsun1@users.noreply.github.com>
2022-03-15 18:33:27 +0300

    Add files via upload

---
+++ minepkt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/packetcrypt ann -p pkt1q4yunj4q7m6us8lv8znvm4wy9ywk8l6xss8447r http://pool.pkt.world http://pool.chymera.it http://pool.pktpool.io http://pktco.in http://pool.pkteer.com http://pool-pkt.dlinodes.com  2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
